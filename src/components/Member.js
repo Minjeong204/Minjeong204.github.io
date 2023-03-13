cd8f548e22b8268eb82674d1aafd9bdef58b02c5
Minjeong204 <anjell1218@gmail.com>
2023-03-13 17:12:59 +0900

    dateFormat

--- src/components/Member.js
+++ src/components/Member.js
@@ -7,12 +7,35 @@ export default function DataTable() {
   useEffect(() => {
     request.get("/table").then((response) => setTableData(response.data));
   });
-  console.log(tableData);
   const columns = [
     { field: "userId", headerName: "ID" },
-    { field: "name", headerName: "NAME" },
-    { field: "regiDt", headerName: "등록일" },
-    { field: "updaDt", headerName: "수정일" },
+    { field: "userNm", headerName: "NAME" },
+    { field: "deptCd", headerName: "이게 뭐지" },
+    { field: "teleNo", headerName: "전화번호", width: 150 },
+    {
+      field: "regiDt",
+      headerName: "등록일",
+      width: 150,
+      valueFormatter: (params) => {
+        if (params.value == null) {
+          return "";
+        }
+        const valueFormatted = params.value.slice(0, 10);
+        return valueFormatted;
+      },
+    },
+    {
+      field: "updaDt",
+      headerName: "수정일",
+      width: 150,
+      valueFormatter: (params) => {
+        if (params.value == null) {
+          return "";
+        }
+        const valueFormatted = params.value.slice(0, 10);
+        return valueFormatted;
+      },
+    },
   ];
   return (
     <div
